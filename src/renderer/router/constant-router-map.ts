7e6011f7ee5bef0cbca31bd0f9a78ef16a871b7e
luongld.it@gmail.com <luongld.it@gmail.com>
2024-09-21 13:10:48 +0700

    save point

--- src/renderer/router/constant-router-map.ts
+++ src/renderer/router/constant-router-map.ts
@@ -2,12 +2,10 @@ import {RouteRecordRaw} from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
     {path: '/:pathMatch(.*)*', component: () => import("@renderer/views/404.vue")},
-    {path: '/', name: 'Home', component: () => import('@renderer/views/home/HomePage.vue')},
+    {path: '/', name: 'Home', component: () => import('@renderer/views/dialer/Dialer.vue')},
     {path: '/active-call', name: 'Active Call', component: () => import('@renderer/views/call/ActiveCall.vue')},
     {path: '/incoming-call', name: 'Incoming Call', component: () => import('@renderer/views/call/IncomingCall.vue')},
     {path: '/outgoing-call', name: 'Outgoing Call', component: () => import('@renderer/views/call/OutgoingCall.vue')},
-
-    {path: '/test', name: 'Test Route', component: () => import('@renderer/views/dialer/Dialer.vue')},
 ]
 
 export default routes
